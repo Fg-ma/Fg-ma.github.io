a2c85eed0bbf03fa3c60819f7c37f693d74f909c
Fg-ma <fgeqma@gmail.com>
2023-06-12 11:55:27 -0700

    Fixed Dots

--- reboot/rStatic/rjs/rCollapsible.js
+++ reboot/rStatic/rjs/rCollapsible.js
@@ -1,14 +1,62 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+coll[0].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var content = document.getElementById('programingCoding');
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  };
+});
+
+coll[1].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var content = document.getElementById('cad');
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  };
+});
+
+coll[2].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var content = document.getElementById('audio');
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  };
+});
+
+coll[3].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var content = document.getElementById('images');
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  };
+});
+
+coll[4].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var content = document.getElementById('launchers');
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  };
+});
+
+coll[5].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var content = document.getElementById('games');
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  };
+});
