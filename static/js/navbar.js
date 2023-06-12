5e9adec7bcfcd3f733be7593de30f5daba781e34
Fg-ma <fgeqma@gmail.com>
2023-06-11 17:52:13 -0700

    fix main page margins

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -2,10 +2,10 @@
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      document.getElementById("navbar").style.padding = "20px 10px";
+      document.getElementById("nexusTitle").style.fontSize = "200%";
+    } else {
       document.getElementById("navbar").style.padding = "30px 10px";
       document.getElementById("nexusTitle").style.fontSize = "300%";
-    } else {
-      document.getElementById("navbar").style.padding = "60px 10px";
-      document.getElementById("nexusTitle").style.fontSize = "450%";
     }
 }
